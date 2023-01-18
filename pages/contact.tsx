7ac9f4b51d60d64bc52054a36ebf7807905b9136
William Carter <whcarter3@users.noreply.github.com>
2023-01-18 17:24:44 -0600

    adds titles to pages

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,31 +1,37 @@
 import Layout from '@/components/layout';
 import ContactForm from '@/components/contactForm';
 import Button from '@/components/button';
+import Head from 'next/head';
 
 const Contact = (): JSX.Element => {
   return (
-    <Layout>
-      <div className="grid grid-cols:1 md:grid-cols-2 gap-16 md:gap-5">
-        <div>
-          <h1 className="text-4xl">Drop me a message!</h1>
-          <ContactForm />
-        </div>
-        <div>
-          <h1 className="text-4xl mb-5">Sign the guestbook!</h1>
-          <p className="text-2xl">
-            You must authenticate with Github to sign the guestbook!
-          </p>
-          <Button className="mt-3">Login</Button>
-          <div className="border-b-2 border-brand-darker mt-5 pb-2">
-            <p className="text-xl">What a dope site!</p>
-            <p className="text-sm">
-              <span className="italic">Anonymous </span> | 17 Jan 2022
-              @ 12:59 am
+    <>
+      <Head>
+        <title>Contact Me</title>
+      </Head>
+      <Layout>
+        <div className="grid grid-cols:1 md:grid-cols-2 gap-16 md:gap-5">
+          <div>
+            <h1 className="text-4xl">Drop me a message!</h1>
+            <ContactForm />
+          </div>
+          <div>
+            <h1 className="text-4xl mb-5">Sign the guestbook!</h1>
+            <p className="text-2xl">
+              You must authenticate with Github to sign the guestbook!
             </p>
+            <Button className="mt-3">Login</Button>
+            <div className="border-b-2 border-brand-darker mt-5 pb-2">
+              <p className="text-xl">What a dope site!</p>
+              <p className="text-sm">
+                <span className="italic">Anonymous </span> | 17 Jan
+                2022 @ 12:59 am
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 };
 
--- pages/works.tsx
+++ pages/works.tsx
@@ -1,11 +1,17 @@
+import Head from 'next/head';
 import Layout from '@/components/layout';
 
 const Works = (): JSX.Element => {
   return (
-    <Layout>
-      <h1 className="text-4xl">My Work</h1>
-      <p>This is a page about my work</p>
-    </Layout>
+    <>
+      <Head>
+        <title>My Works</title>
+      </Head>
+      <Layout>
+        <h1 className="text-4xl">My Work</h1>
+        <p>This is a page about my work</p>
+      </Layout>
+    </>
   );
 };
 
