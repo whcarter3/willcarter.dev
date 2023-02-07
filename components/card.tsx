225dacaed57e22c53d2ef13de88d9d3757b8887b
William Carter <whcarter3@users.noreply.github.com>
2023-02-06 20:29:14 -0800

    addds boolean for card component, finalizes card design

--- components/card.tsx
+++ components/card.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { FaLinkedin } from 'react-icons/fa';
 import classNames from 'classnames';
+import useGradient from '@/hooks/useGradient';
 
 interface CardProps {
   image: string;
   title: string;
   description: string;
   link: string;
-  linkText: string;
   imgSize: number;
   className?: string;
+  reverse?: boolean;
 }
 
 const Card = ({
@@ -17,31 +19,51 @@ const Card = ({
   title,
   description,
   link,
-  linkText,
   imgSize,
   className,
+  reverse,
 }: CardProps) => {
+  const [gradient, handleMove, ref] = useGradient<HTMLDivElement>();
   return (
-    <div className="card rounded-md">
-      <div className="flex">
+    <div
+      ref={ref}
+      style={{ background: gradient }}
+      onMouseMove={handleMove.onMouseMove}
+      onTouchMove={handleMove.onTouchMove}
+      className="card rounded-md flex"
+    >
+      <div
+        className={classNames(
+          reverse ? 'order-2' : 'order-1',
+          'shrink-0'
+        )}
+      >
         <Image
           src={image}
           alt={title}
           width={imgSize}
           height={imgSize}
-          className={classNames('rounded-md mr-5', className)}
+          className={classNames(
+            'rounded-md',
+            className,
+            reverse ? 'ml-5' : 'mr-5'
+          )}
         />
-        <div>
+        <div
+          className={classNames(
+            'flex flex-col',
+            reverse ? 'items-end' : 'items-start'
+          )}
+        >
           <h3 className="text-2xl">{title}</h3>
-          <Link
-            href={link}
-            className="border-brand-darker border-b-2 hover-shadow"
-          >
-            {linkText}
+          <Link href={link} className="hover-shadow">
+            <FaLinkedin />
           </Link>
         </div>
       </div>
-      <p className="mt-3">{description}</p>
+      <p className={classNames(reverse ? 'order-1' : 'order-2')}>
+        {description}
+      </p>
     </div>
   );
 };
--- pages/about.tsx
+++ pages/about.tsx
@@ -11,8 +11,7 @@ const Endorsements = [
     description:
       'Will made an immediate impact on our team and the projects he was involved in from his very first day. Will has an incredible ability to pick up new things fast and master them. On top of that, his curious mind allowed us to solve issues and build better products. His attention to detail and dedication were invaluable to the success of our goals. He is also a great colleague that will bring not only knowledge, but also teamwork, positivity, and joy, creating a positive environment to work in. Will would be an incredible asset to any team, project, and company.',
     link: 'https://www.linkedin.com/in/gabriel-perez-a703521a/',
-    linkText: 'LinkedIn Profile',
-    imgSize: 150,
+    imgSize: 200,
   },
   {
     image: '/images/treyWhitson.jpeg',
@@ -20,8 +19,8 @@ const Endorsements = [
     description:
       'I had the pleasure of having Will as a direct report for over 6 months at Mythical Games. Will is driven, knowledgeable, and has a true passion for what he does. Will has a knack for working and communicating with both technical and non-technical colleagues, being very thorough when gathering information, communicating to others, and executing. Not only is he an efficient contributor, he is sincerely a great person to work with.',
     link: 'https://www.linkedin.com/in/trey-whitson/',
-    linkText: 'LinkedIn Profile',
-    imgSize: 150,
+    imgSize: 200,
+    reverse: true,
   },
   {
     image: '/images/tomBoatman.jpeg',
@@ -29,8 +28,7 @@ const Endorsements = [
     description:
       'Working with Will was a pleasure. I found his enthusiasm and creativity to be most intoxicating. He is constantly thinking of new and beneficial ways to approach work. When faced with a problem, he seeks an ideal solution and pursues it relentlessly. His strongest talents are visual storytelling, an ability to connect with people, and goal-oriented focus. Not only is he good with people, but he has an uncanny ability to identify a person’s needs and inspire them to reach their goals. He is enthusiastic about challenges and seeks perfection, but he is always willing to adapt his high standards to meet varying project parameters and client demands. I believe he would be an asset to any creative team.',
     link: 'https://www.linkedin.com/in/native/',
-    linkText: 'LinkedIn Profile',
-    imgSize: 150,
+    imgSize: 200,
   },
 ];
 
@@ -102,10 +100,10 @@ const About = (): JSX.Element => {
           </div>
         </div>
         <div>
-          <h2 className="text-4xl mb-3 mt-5 text-center">
+          <h2 className="text-4xl mb-5 mt-5 text-center">
             Endorsements
           </h2>
-          <div className="grid grid-cols-3 gap-5">
+          <div className="grid grid-cols-1 gap-5 max-w-[800px] mx-auto">
             {Endorsements.map((item, index) => (
               <Card
                 key={index}
@@ -113,9 +111,9 @@ const About = (): JSX.Element => {
                 image={item.image}
                 imgSize={item.imgSize}
                 link={item.link}
-                linkText={item.linkText}
                 title={item.title}
                 className="rounded-md"
+                reverse={item.reverse}
               />
             ))}
           </div>
